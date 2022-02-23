4b68932f627ce9fc6e8f06a06fc28ef7bc80d92d
Julius0505 <se7en.mini.123@protonmail.com>
2022-02-23 17:20:16 +0300

    add loading

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,14 @@ import Search from './components/search'
 import axios, { AxiosRequestConfig } from 'axios'
 import WebpageShow from './components/results/webpageshow'
 import NewsShow from './components/results/newsshow'
-import { Box, Grid } from '@mui/material'
+import { Box, CircularProgress, Grid } from '@mui/material'
 import RelatedShow from './components/results/relatedshow'
 
 const BING_ENDPOINT = 'https://api.bing.microsoft.com/v7.0/search'
 
 function App() {
   const [data, setData] = useState<any>(null)
+  const [loading, setLoading] = useState<boolean>(false)
 
   const handleSearch = (value: string) => {
     const config: AxiosRequestConfig = {
@@ -21,38 +22,44 @@ function App() {
       },
       params: { q: value }
     }
+    setLoading(true)
     axios
       .get(BING_ENDPOINT, config)
       .then((res) => {
-        setData(res)
+        setData(res.data)
+        setLoading(false)
       })
-      .catch((err) => console.error(err))
+      .catch((err) => setLoading(false))
   }
 
   return (
     <div className="App">
       <Search onSearch={handleSearch} />
 
-      <Grid container spacing={2}>
-        <Grid item xs={8}>
-          {data?.rankingResponse.mainline.items.map((item: any, i: number) => (
-            <Box key={i} sx={{ m: 2 }}>
-              {item.answerType === 'WebPages' ? (
-                <WebpageShow data={data.webPages.value[item.resultIndex]} />
-              ) : item.answerType === 'News' ? (
-                <NewsShow data={data.news.value} />
-              ) : item.answerType === 'RelatedSearches' ? (
-                <RelatedShow data={data.relatedSearches.value} />
-              ) : (
-                <></>
-              )}
-            </Box>
-          ))}
+      {loading ? (
+        <CircularProgress />
+      ) : (
+        <Grid container spacing={2}>
+          <Grid item xs={8}>
+            {data?.rankingResponse.mainline.items.map(
+              (item: any, i: number) => (
+                <Box key={i} sx={{ m: 2 }}>
+                  {item.answerType === 'WebPages' ? (
+                    <WebpageShow data={data.webPages.value[item.resultIndex]} />
+                  ) : item.answerType === 'News' ? (
+                    <NewsShow data={data.news.value} />
+                  ) : item.answerType === 'RelatedSearches' ? (
+                    <RelatedShow data={data.relatedSearches.value} />
+                  ) : (
+                    <></>
+                  )}
+                </Box>
+              )
+            )}
+          </Grid>
+          <Grid item xs={4}></Grid>
         </Grid>
-        <Grid item xs={4}>
-          Sideline
-        </Grid>
-      </Grid>
+      )}
     </div>
   )
 }
